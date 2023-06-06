0327734c0326d990a36214bddca458e7fd978b3e
M <fwtmagan@gmail.com>
2023-06-06 12:00:18 +0900

    0606

--- StickerSmash/page/second.js
+++ StickerSmash/page/second.js
@@ -1,12 +1,46 @@
-import { Button, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, Text, Platform } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
-function SecondScreen({ navigation }) {
+export default function SecondScreen() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images, // All 包含相片、影像
+      allowsEditing: true, // 允許編輯
+      allowsMultipleSelection: true, // 允許多張選取
+      aspect: [4, 3], // 比例
+      quality: 1, // 壓縮比例 數值0-1（小數點也可設定）
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Second Screen</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Second Screen</Text>
+        <Button title="Go back" onPress={() => navigation.goBack()} />{" "}
+      </View>
     </View>
   );
 }
 
-export default SecondScreen;
+// function SecondScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Second Screen</Text>
+//       <Button title="Go back" onPress={() => navigation.goBack()} />
+//     </View>
+//   );
+// }
