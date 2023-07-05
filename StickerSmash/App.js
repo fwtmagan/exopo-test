b57cc7330f7a0183bcce8a79427eb9afc57598c8
M <fwtmagan@gmail.com>
2023-07-05 14:42:04 +0900

    add css

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,67 +1,28 @@
 // In App.js in a new project
 
 import * as React from "react";
-import { Button, View, Text } from "react-native";
-import { useEffect, useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./page/home";
 import SecondScreen from "./page/second";
 
-// function HomeScreen({ navigation }) {
-//   const [count, setCount] = useState(0); //カウント保存用
-//   useEffect(() => {
-//     (async () => {
-//       const count = await AsyncStorage.getItem("count"); // 保存されたcountを取得する処理
-//       setCount(Number(count || 0) + 1); // Numberに変換してインクリメントする
-//     })();
-//   }, []);
-//   useEffect(() => {
-//     if (count) {
-//       AsyncStorage.setItem("count", String(count)); // 文字列型に変換して保存する
-//     }
-//   }, [count]);
-
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Home Screen</Text>
-//       <Text>{`${count}`}</Text>
-//       <Button title="Next." onPress={() => navigation.navigate("Details")} />
-//     </View>
-//   );
-// }
-
-// function DetailsScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Details Screen</Text>
-//       <Button
-//         title="Go to Details... again"
-//         onPress={() => navigation.push("Details")}
-//       />
-//       <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-//       <Button title="Go back" onPress={() => navigation.goBack()} />
-//       <Button
-//         title="Go back to first screen in stack"
-//         onPress={() => navigation.popToTop()}
-//       />
-//     </View>
-//   );
-// }
-
 function App() {
   const Stack = createNativeStackNavigator();
+
+  const screenOptions = {
+    headerStyle: {
+      backgroundColor: "#D0F6FE",
+    },
+  };
+
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Second" component={SecondScreen} />
-          {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- StickerSmash/page/home.js
+++ StickerSmash/page/home.js
@@ -1,4 +1,10 @@
-import { Button, View, Text } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -10,6 +16,7 @@ function HomeScreen({ navigation }) {
       setCount(Number(count || 0) + 1); // Numberに変換してインクリメントする
     })();
   }, []);
+
   useEffect(() => {
     if (count) {
       AsyncStorage.setItem("count", String(count)); // 文字列型に変換して保存する
@@ -18,10 +25,59 @@ function HomeScreen({ navigation }) {
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <ImageBackground
+        source={require("../assets/ceiling.png")}
+        style={styles.container}
+      ></ImageBackground>
       <Text>HOME {`${count}`}</Text>
-      <Button title="Screen2" onPress={() => navigation.navigate("Second")} />
+      <TouchableOpacity
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          shadowColor: "black",
+          shadowOffset: {
+            width: 0,
+            height: 3,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 5,
+          elevation: 3,
+          backgroundColor: "#D9D9D9",
+          borderRadius: 5,
+          height: 35,
+          width: 105,
+          marginHorizontal: 50,
+          marginVertical: 10,
+        }}
+        onPress={() => navigation.navigate("Second")}
+      >
+        <Text style={{ marginHorizontal: 20, color: "black" }}>終了</Text>
+      </TouchableOpacity>
+      <ImageBackground
+        source={require("../assets/wallpaper.png")}
+        style={styles.backgroundImage}
+      ></ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 792,
+    height: 135,
+    flex: 3,
+    alignItems: "center",
+    justifyContent: "center",
+    resizeMode: "repeat",
+  },
+  backgroundImage: {
+    width: 792,
+    height: 170,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    resizeMode: "repeat",
+  },
+});
 
 export default HomeScreen;
